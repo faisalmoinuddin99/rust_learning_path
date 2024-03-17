204145ac3a83e29c98751b861fd540d180b692ed
faisalmoinuddin99 <faisal25march99@gmail.com>
2024-03-17 16:18:20 +0530

    First Part

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,22 @@
+use std::io ;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!") ;
+
+    println!("Please input your guess :") ;
+
+    let apple = 5 ; // immutable
+    let mut banana = 12 ; // mutable
+
+
+    let mut guess = String::new() ;
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line") ;
+
+    println!("You guessed: {guess}") ;
+
+    let total = apple + banana ;
+    println!("Total = {total}")
 }
