c94eb75633dc25e4cd5ec71d8aac0e1e9962bf44
faisalmoinuddin99 <faisal25march99@gmail.com>
2024-03-28 16:43:59 +0530

    Numerical Operation

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,11 +1,12 @@
-fn main() {
-    let x = 5 ;
-    let x = x + 1 ;
-    {
-        let x = x * 2 ;
-        println!("The value of x in the inner scope: {x} ")
 
-        // inner shadowing ends
-    }
-    println!("The value of x in the outer scope: {x}")
+
+fn main() {
+    let mut age = 19 ;
+
+     age = 20 ;
+
+    let  spaces = "   ";
+    let spaces = spaces.len() ;
+
+    println!("Your age sir: {age}, {spaces}")
 }
